c1b8bb263d58ee6d6acf70702c9beac931bb2d0e
mahfuz091 <engmahfuz14@gmail.com>
2025-10-24 23:50:39 +0600

    'add'

--- merchant/src/app/dashboard/add-design/page.jsx
+++ merchant/src/app/dashboard/add-design/page.jsx
@@ -1,14 +1,10 @@
 import { getAllBrands } from "@/app/actions/brand/brand.actions";
 import { getAllMockups } from "@/app/actions/mockup/mockup.actions";
 import { auth } from "@/auth";
-import AddDesign from "@/components/AddDesign/AddDesign";
-import AddDesign3 from "@/components/AddDesign/AddDesign3";
-import AddDesignC from "@/components/AddDesign/AddDesignC";
-import AddDesignFit from "@/components/AddDesign/AddDesignFit";
+
 import AddDesignFitNew from "@/components/AddDesign/AddDesignFitNew";
-import AddDesignFunction from "@/components/AddDesign/AddDesignFunction";
-import AddDesignNew from "@/components/AddDesign/AddDesignNew";
-import Layout from "@/components/Layout/Layout";
+
+
 import React from "react";
 
 const page =async () => {

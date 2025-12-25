faf1cddb1088a37a28f35d2a17b3835d91f7ddc3
mahfuz091 <engmahfuz14@gmail.com>
2025-12-25 22:44:08 +0600

    prisma define

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import HeaderCloned from "../Header/HeaderCloned";
+import prisma from "@/lib/prisma";
 
 const Layout = async ({ children, session }) => {
   const categories = await prisma.productCategory.findMany();
